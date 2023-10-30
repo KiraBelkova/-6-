b169b20fea0a75165d53999f6bc8a3d14b508a2b
Кира Белькова <kirabelkova2013@yandex.ru>
2023-10-30 14:57:31 +0300

    Добавила решение задачи1 к Семинару6

---
+++ task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите числа через пробел: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int res = 0;
+
+for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+        if(arr[i] > 0)
+        {
+            res++;
+        }
+    }
+
+Console.WriteLine();
+Console.WriteLine("Чисел больше нуля: " + res);
